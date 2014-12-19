47a3e219dcf3a6712c7e5733dd42d20bca7eaaa7
yakhyoev <yakhyoev@gmail.com>
2014-12-19 09:33:46 +0500

    Said Yakhyoev's updated cacheMatrix r file
    my code for calculating cache matrix

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,40 @@
-## Put comments here that give an overall description of what your
-## functions do
+#Said Yakhyoev
+#invertable matrix functions
 
-## Write a short comment describing this function
 
-makeCacheMatrix <- function(x = matrix()) {
+# this function takes a matrix or a numeric vector and returns a constructed a square matrix.
+# the constructed matrix is saved, later to be checked if a repeat request is being processed
 
+makeCacheMatrix <- function(matrix1 = ()) {
+    cache <<- NULL                  # if makeCacheMatrix is called, cache needs re-caclc. Empty cache, save step in next func
+    mat_side = sqrt(length(matrix1))
+    cached_matrix <<-matrix1        # this is to check later if original input has changed
+    constructed_matrix <- matrix(matrix1, nrow = mat_side, ncol = mat_side)
+    return(constructed_matrix)
+    
 }
 
-
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+# Solves matrix or returns existing cached result
+# first checks if cache already exists and whether a pre-exisitn matrix is being requested again
+cacheSolve <- function(matrix1,...){
+    
+    # if cache exists, check that result of the same matrix is requested. Return existing cache
+    if (!is.null(cache)) {
+        if (identical(matrix1, cached_matrix)){
+            print('Using existing cached data:')
+            return (cache)
+        }
+        # if cache exist, but a calculation of a new matrix is needed, re-calculate
+        else{
+            print('Recalculating with a new matrix:')
+            cache <<- solve(makeCacheMatrix(matrix1))
+            return(cache)
+        }        
+    }
+    # if cache is altogether empty, perform calculation
+    else {   
+        print('Cache is empty. Solving inverse matrix:')
+        cache <<- solve(makeCacheMatrix(matrix1)) 
+        return (cache)
+    }
 }
